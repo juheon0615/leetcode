2c263cba9b154d742f7a0fd2a8795b25b4fe64e0
juheon0615 <juheon0615@gmail.com>
2023-06-10 20:43:43 +0700

    Time: 54 ms (64.57%), Space: 17.2 MB (9.77%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        '''
+        idea... keep the index of incomplete parentheses
+        calculate the largest diff between the incomplete parentheses
+        '''
+        
+        
+        stack = []
+        
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            else:
+                if len(stack) > 0 and s[stack[-1]] == "(":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            
+        
+        if len(stack) == 0:
+            return len(s)
+        
+        
+        ret = 0
+        n = len(s)
+        while stack:
+            ret = max(ret, n - stack[-1] - 1)
+            n = stack[-1]
+            stack.pop()
+        
+        return max(ret, n)
+        
+        
+        
+        
