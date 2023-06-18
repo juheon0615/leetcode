cb11e3b17cbd263d48dada997fe3449c0f8a6b07
juheon0615 <juheon0615@gmail.com>
2023-06-18 16:51:33 +0700

    Time: 47 ms (44.50%), Space: 16.3 MB (56.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,34 +1,36 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        '''
-
-        1 1 1 
-        1 2
-        2 1
+        fib = []
+        fib.append(0)
+        fib.append(1)
+        fib.append(2)
         
+        for i in range(3, n+1):
+            fib.append(fib[i-1] + fib[i-2])
         
-        '''
+        return fib[n]
+            
         
-        mem = {}
-        def dp(n):
-            if n in mem:
-                return mem[n]
+#         mem = {}
+#         def dp(n):
+#             if n in mem:
+#                 return mem[n]
             
-            if n == 0:
-                return 1
+#             if n == 0:
+#                 return 1
             
-            if n < 0:
-                return 0
+#             if n < 0:
+#                 return 0
             
-            ret = 0
-            for i in range(1, 3):
-                ret += dp(n - i)
+#             ret = 0
+#             for i in range(1, 3):
+#                 ret += dp(n - i)
             
             
-            mem[n] = ret
-            return ret
+#             mem[n] = ret
+#             return ret
     
-        return dp(n)
+#         return dp(n)
         
         
                 
