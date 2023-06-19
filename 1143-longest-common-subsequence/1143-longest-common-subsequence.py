6bc4172de0d74ab0c9087b2942e494dadcd9b89e
juheon0615 <juheon0615@gmail.com>
2023-06-19 19:13:55 +0700

    Time: 1445 ms (24.32%), Space: 44.8 MB (22.80%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,15 +1,41 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        t = [[0 for t2 in range(len(text2) + 1)] for t1 in range(len(text1) + 1) ]
+#         t = [[0 for t2 in range(len(text2) + 1)] for t1 in range(len(text1) + 1) ]
         
-        for i in range(1, len(text1) + 1):
-            for j in range(1, len(text2) + 1):
-                if text1[i-1] == text2[j-1]:
-                    t[i][j] = t[i-1][j-1] + 1
-                else:
-                    t[i][j] = max(t[i-1][j], t[i][j-1])
+#         for i in range(1, len(text1) + 1):
+#             for j in range(1, len(text2) + 1):
+#                 if text1[i-1] == text2[j-1]:
+#                     t[i][j] = t[i-1][j-1] + 1
+#                 else:
+#                     t[i][j] = max(t[i-1][j], t[i][j-1])
         
-        return t[len(text1)][len(text2)]
+#         return t[len(text1)][len(text2)]
+    
+        
+        mem = [[-1 for t2 in range(len(text2))] for t1 in range(len(text1))]
+        
+        def lcs(text1, text2, t1, t2):
+            if t1 == len(text1) or t2 == len(text2):
+                return 0
+            
+            if mem[t1][t2] != -1:
+                return mem[t1][t2]
+            
+            ret = 0
+            
+            if text1[t1] == text2[t2]:
+                ret = lcs(text1, text2, t1 + 1, t2 + 1) + 1
+            else:
+                ret = max(lcs(text1, text2, t1 + 1, t2), lcs(text1, text2, t1, t2 + 1))
+            
+            mem[t1][t2] = ret
+            
+            return ret
+        
+        answer = lcs(text1, text2, 0, 0)
+        # print(mem)
+        return answer
+         
     
     '''
     memoization version
