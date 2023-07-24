f2450bdbaf925bac30f77b6b2e661ca59bb7a418
juheon0615 <juheon0615@gmail.com>
2023-07-25 00:29:48 +0700

    Time: 61 ms (41.45%), Space: 17.4 MB (97.72%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,36 +6,51 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        if root is None:
-            return False
-        
-        #bfs
-        nodes = [(root, targetSum)]
-        ret = False
-        
-        while nodes:
-            nextNodes = []
-            while nodes:
-                node, target = nodes.pop(0)
-                
-                if node.right is None and node.left is None and target - node.val == 0:
-                    ret = True
-                    break
-                
-                if node.right is not None:
-                    nextNodes.append((node.right, target - node.val))
-                if node.left is not None:
-                    nextNodes.append((node.left, target - node.val))
+                    
+        def dfs(node, target):
+            if node.left is None and node.right is None and target - node.val == 0:
+                return True
             
-            nodes = nextNodes
+            ret = False
+            if node.right is not None:
+                ret = ret or dfs(node.right, target - node.val)
+            
+            if node.left is not None:
+                ret = ret or dfs(node.left, target - node.val)
+            
+            return ret
+        
+        return dfs(root, targetSum) if root is not None else False
+    
+#         if root is None:
+#             return False
+        
+#         #bfs
+#         nodes = [(root, targetSum)]
+#         ret = False
+        
+#         while nodes:
+#             nextNodes = []
+#             while nodes:
+#                 node, target = nodes.pop(0)
+                
+#                 if node.right is None and node.left is None and target - node.val == 0:
+#                     ret = True
+#                     break
+                
+#                 if node.right is not None:
+#                     nextNodes.append((node.right, target - node.val))
+#                 if node.left is not None:
+#                     nextNodes.append((node.left, target - node.val))
+            
+#             nodes = nextNodes
                 
             
-            if ret == True:
-                break
+#             if ret == True:
+#                 break
                 
         
-        return ret
+#         return ret
                 
                 
                 
