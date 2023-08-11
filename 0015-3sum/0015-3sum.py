9e1e9bb7c8640bc98597f18980a79fb45f79ce58
juheon0615 <juheon0615@gmail.com>
2023-08-12 00:03:08 +0700

    Time: 2164 ms (33.37%), Space: 20.5 MB (40.94%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # on brute force i,j,k O(n^3)
-        # sort then 2 cursor, O(nlogn) + O(n^2)
-        
+    
         ret = []
-        retSet = set()
-        
         nums.sort()
-        prevNum = None
-        for i in range(0, len(nums) - 2):
-            if nums[i] == prevNum:
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            j = i+1
+            j = i + 1
             k = len(nums) - 1
             
-            while j < k:                
+            while j < k:
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    j += 1
+                    continue
+                
+                if k < len(nums) - 2 and nums[k] == nums[k+1]:
+                    k -= 1
+                    continue
+                
                 s = nums[i] + nums[j] + nums[k]
-                if s > 0:
+                
+                if s == 0:
+                    ret.append([nums[i], nums[j], nums[k]])
+                    j += 1
                     k -= 1
                 elif s < 0:
                     j += 1
                 else:
-                    key = "%d %d %d" % (nums[i], nums[j], nums[k])
-                    if key not in retSet:
-                        ret.append([nums[i], nums[j], nums[k]])
-                        retSet.add(key)
                     k -= 1
-                    j += 1
-            
-            prevNum = nums[i]
         
         return ret
-            
