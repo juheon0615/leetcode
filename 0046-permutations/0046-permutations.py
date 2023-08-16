4dbb360df3eda1ad3206b81e556343788a1bc5ae
juheon0615 <juheon0615@gmail.com>
2023-08-16 12:46:18 +0900

    Time: 38 ms (93.89%), Space: 16.3 MB (89.61%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,33 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-#         ret = []
         
-#         def dfs(nums, cur):
-#             if len(nums) == 0:
-#                 ret.append(cur)
-#                 return
-#             for i in range(len(nums)):
-#                 dfs(nums[:i] + nums[i+1:], cur[:] + [nums[i]])
-        
-#         dfs(nums,[])
-        # using counter for memory
-    
-    
-    
         ret = []
-        def dfs(counter, cur):
-            if len(cur) == len(nums):
+        n = len(nums)
+        def dfs(cur, nums):
+            # print(cur, " : ", nums)
+            if len(cur) == n:
                 ret.append(cur)
                 return
             
-            for x in counter:
-                if counter[x] > 0:
-                    counter[x] -= 1
-                    dfs(counter, cur + [x])
-                    counter[x] += 1
-              
-        dfs(Counter(nums), [])
-                
+            
+            for i, num in enumerate(nums):
+                dfs(cur[:] + [num], nums[:i] + nums[i+1:])
+            
+        dfs([],nums)
+        
         return ret
+                
                 
                 
