bd69b51e0c9241a4e0eb91b94085b57cdd251762
juheon0615 <juheon0615@gmail.com>
2023-08-16 11:00:35 +0900

    Time: 1139 ms (5.03%), Space: 22.3 MB (73.60%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,30 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        ret = 0
+        left = 0
+        cnts = {}
+        left = 0
+        
+        for i, fruit in enumerate(fruits):
+            
+            if fruit not in cnts:
+                cnts[fruit] = 0
+            
+            cnts[fruit] += 1
+            print(cnts)
+            
+            while len(cnts) > 2:
+                f = fruits[left]
+                cnts[f] -= 1
+                if cnts[f] == 0:
+                    del cnts[f]
+                left += 1
+            
+            ret = max(ret, i - left + 1)
+            
+                
+            
+        
+        return ret
+        
+        
