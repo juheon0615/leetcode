79832714efa3bfa361d844080b692af3f54c5f0f
juheon0615 <juheon0615@gmail.com>
2023-10-27 12:47:12 +0900

    Time: 755 ms (95.71%), Space: 16.5 MB (95.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,39 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        def dp(amt, mem):
-            if amt in mem:
-                return mem[amt]
-            
-            if amt == 0:
-                return True, 0
-            
-            found = False
-            ret = None
-            
-            for coin in coins:
-                if amt >= coin:
-                    f, r = dp(amt-coin, mem)
-                    if f is True:
-                        if ret is None:
-                            found = f
-                            ret = r + 1
-                        else:
-                            found = f
-                            ret = min(ret, r + 1)
-            
-            mem[amt] = (found, ret)
-            
-            return mem[amt]
-        
-        mem = {}
-        
-        found, ret = dp(amount, mem)
-        # print(mem)
-        return ret if found == True else -1
+        dp = [None for _ in range(amount+1)]
         
         
         
-                        
+        dp[0] = 0
+        
+        for coin in coins:
+            for c in range(coin, amount+1):
+                if dp[c] is None:
+                    dp[c] = dp[c] if dp[c-coin] is None else dp[c-coin] + 1
+                else:
+                    dp[c] = dp[c] if dp[c-coin] is None else min(dp[c-coin] + 1, dp[c])
+
+        return dp[amount] if dp[amount] is not None else -1
             
             
