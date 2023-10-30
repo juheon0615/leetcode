e1973b09e98bed949c7958f6f0a9ecd27ef6f7d9
juheon0615 <juheon0615@gmail.com>
2023-10-30 10:21:06 +0900

    Time: 674 ms (97.05%), Space: 16.5 MB (95.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [None for _ in range(amount+1)]
+        
+        changes = [None for _ in range(amount + 1)]
         
         
-        
-        dp[0] = 0
+        changes[0] = 0
         
         for coin in coins:
-            for c in range(coin, amount+1):
-                if dp[c] is None:
-                    dp[c] = dp[c] if dp[c-coin] is None else dp[c-coin] + 1
-                else:
-                    dp[c] = dp[c] if dp[c-coin] is None else min(dp[c-coin] + 1, dp[c])
-
-        return dp[amount] if dp[amount] is not None else -1
-            
-            
+            for i in range(coin, amount + 1):
+                if changes[i - coin] is not None:
+                    if changes[i] is None:
+                        changes[i] = changes[i-coin] + 1
+                    else:
+                        changes[i] = min(changes[i-coin] + 1, changes[i])
+        
+        return -1 if changes[amount] is None else changes[amount]
