d8e304de1f371bfef872a25d21d459332e6e431b
juheon0615 <juheon0615@gmail.com>
2024-03-04 18:27:29 +0900

    Time: 44 ms (91.10%), Space: 16.6 MB (89.50%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,46 +1,27 @@
 class Solution:
-    def dfs(self, nums, cur, target, ret):
-        if target == 0:
-            ret.append(cur)
-            return
-
-        if target < 0:
-            return
-
-
-        for i, num in enumerate(nums):
-            self.dfs(nums[i:], cur[:] + [nums[i]], target - nums[i], ret)
-            
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        # ret = []
-        # self.dfs(candidates, [], target, ret)
+        result = []
         
-        ret = []
-        q = []
         
-        # [current start i, cur comb, remaining target]
-        
-        q.append([0, [], target])
-        while q:
-            nextQ = []
+        def bt(cur, sums, ii):
+            if sums == target:
+                result.append(list(cur))
+                return
+            elif sums > target:
+                return
             
-            while q:
-                c = q.pop()
-                i = c[0]
-                comb = c[1]
-                t = c[2]
+            for i in range(ii, len(candidates)):
+                cur.append(candidates[i])
                 
-                if c[2] == 0:
-                    ret.append(c[1])
-                elif c[2] < 0 :
-                    continue
-                else:
-                    for j in range(i, len(candidates)):
-                        nextQ.append([j, comb[:] + [candidates[j]], t - candidates[j]])
-            
-            q = nextQ
-
-
-        return ret
-            
+                bt(cur, sums + candidates[i], i)
+                
+                cur.pop()
+        
+        bt([], 0, 0)
+        
+        return result
+        
+        
+        
+        
