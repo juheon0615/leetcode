081bcffcce597ab4c602854b1d79427886a6a947
juheon0615 <juheon0615@gmail.com>
2024-06-06 23:26:36 +0900

    Time: 83 ms (43.45%), Space: 16.8 MB (96.12%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,34 +1,38 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        totalCnt = len(nums1) + len(nums2)
-        
-        i1 = i2 = 0
-        merged = []
-        
-        while i1 < len(nums1) or i2 < len(nums2):
-            if i1 == len(nums1):
-                merged.append(nums2[i2])
-                i2 += 1
-            elif i2 == len(nums2):
-                merged.append(nums1[i1])
-                i1 += 1
-            else:
-                if nums1[i1] < nums2[i2]:
-                    merged.append(nums1[i1])
+        cnt = len(nums1) + len(nums2)
+
+        def find_the_nth(nums1, nums2, nth):
+            i1 = i2 = 0
+            ret = []
+
+            while len(ret) <= nth:
+                if i1 == len(nums1):
+                    # print(1)
+                    ret.append(nums2[i2])
+                    i2 += 1
+                elif i2 == len(nums2):
+                    # print(2)
+                    ret.append(nums1[i1])
                     i1 += 1
                 else:
-                    merged.append(nums2[i2])
-                    i2 += 1
-        
-        print(merged)
-        
-        ret = 0.0
-        
-        if len(merged) % 2 == 0:
-            ret = (merged[len(merged) // 2] + merged[(len(merged) // 2) - 1]) / 2
-        else:
-            ret = merged[len(merged) // 2] * 1.0
-        
-        return ret
+                    if nums1[i1] < nums2[i2]:
+                        # print(3)
+                        ret.append(nums1[i1])
+                        i1 += 1
+                    else:
+                        # print(4)
+                        ret.append(nums2[i2])
+                        i2 += 1
+            # print(ret)
+            return ret[-1]
 
+
+        if cnt % 2 == 0:
+            return (find_the_nth(nums1,nums2, cnt//2) + find_the_nth(nums1,nums2, cnt//2 - 1)) /2
+        else:
+            return find_the_nth(nums1,nums2,cnt//2)
+
+            
+        
         
