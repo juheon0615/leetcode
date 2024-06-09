af82d4f95f82781eff06b54136954390a3bab3e5
juheon0615 <juheon0615@gmail.com>
2024-06-10 01:32:16 +0900

    Time: 340 ms (38.93%), Space: 20.9 MB (49.17%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,64 +1,34 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-#         def dfs(i, j):
-#             print(i, " ", j)
-#             if not dp[i][j]:
-#                 val = matrix[i][j]
-#                 dp[i][j] = 1 + max(
-#                     dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
-#                     dfs(i + 1, j) if i < M - 1 and val > matrix[i + 1][j] else 0,
-#                     dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
-#                     dfs(i, j + 1) if j < N - 1 and val > matrix[i][j + 1] else 0)
-#             return dp[i][j]
 
-#         if not matrix or not matrix[0]: return 0
-#         M, N = len(matrix), len(matrix[0])
-#         dp = [[0] * N for i in range(M)]
-#         return max(dfs(x, y) for x in range(M) for y in range(N))
-        
-        def dfs(i, j, dp):
-            if dp[i][j] > 0:
-                return dp[i][j]
-               
-            ret = 0
+        cache = {}
+        def dfs(i, j, m, n):
             
-            #up
-            if i > 0 and matrix[i][j] < matrix[i - 1][j]:
-                ret = max(ret, dfs(i - 1, j, dp))
-            #down
-            if i < len(matrix) - 1 and matrix[i][j] < matrix[i + 1][j]:
-                ret = max(ret, dfs(i + 1, j, dp))
+            if (i,j) in cache:
+                return cache[(i,j)]
             
-            #left
-            if j > 0 and matrix[i][j] < matrix[i][j - 1]:
-                ret = max(ret, dfs(i, j - 1, dp))
-            
-            #right
-            if j < len(matrix[0]) - 1 and matrix[i][j] < matrix[i][j + 1]:
-                ret = max(ret, dfs(i, j + 1, dp))  
-                          
+            ret = 1
 
-            ret += 1
-            dp[i][j] = ret
+            moves = [[1,0], [-1, 0], [0,1], [0, -1]]
 
-            return dp[i][j] 
-        
-        
-        ans = 0
-        if not matrix or not matrix[0]: return 0
-        M, N = len(matrix), len(matrix[0])
-        dp = [[0] * N for i in range(M)]
-        for i in range(M):
-            for j in range(N):
-                ans = max(ans, dfs(i,j, dp))
-        
-        
-        return ans
-                
-                
-        
-                          
+            for move in moves:
+                offi, offj = move
+                ii = i + offi
+                jj = j + offj
 
+                if 0 <= ii < m and 0 <= jj < n and matrix[ii][jj] > matrix[i][j]:
+                    ret = max(ret, 1 + dfs(ii,jj,m,n))
             
-            
+            cache[(i,j)] = ret
+            return ret
         
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ret = 0
+
+        for i in range(m):
+            for j in range(n):
+                ret = max(ret, dfs(i,j,m,n))
+        # print(cache)
+        return ret
