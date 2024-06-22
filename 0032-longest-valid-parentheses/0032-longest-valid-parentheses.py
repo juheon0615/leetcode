541419df5b19566efb0b384b8a11a616b0d36d53
juheon0615 <juheon0615@gmail.com>
2024-06-22 18:39:25 +0900

    Time: 48 ms (27.44%), Space: 17.2 MB (41%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -5,9 +5,7 @@ class Solution:
         calculate the largest diff between the incomplete parentheses
         '''
         
-        
         stack = []
-        
         for i, c in enumerate(s):
             if c == "(":
                 stack.append(i)
@@ -16,21 +14,14 @@ class Solution:
                     stack.pop()
                 else:
                     stack.append(i)
-            
-        
+
         if len(stack) == 0:
             return len(s)
-        
         
         ret = 0
         n = len(s)
         while stack:
             ret = max(ret, n - stack[-1] - 1)
-            n = stack[-1]
-            stack.pop()
+            n = stack.pop()
         
         return max(ret, n)
-        
-        
-        
-        
