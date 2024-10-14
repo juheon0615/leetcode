cd78beddd905b501c1cbd1c0ffc8b559e6955227
juheon0615 <juheon0615@gmail.com>
2024-10-14 10:08:41 +0900

    Time: 527 ms (52.71%), Space: 29.4 MB (68.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,27 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
-        
-        1, 3, 0, 0, 4, 0, 2
-        
-        1,3 => 1
-        1,4 => 4
-        3,4 => 9
-        3,2 => 10
-        4,2 => 4
-        
-        4,2 does not need to be considered. 
-        because 3,2 is the biggest area that can be formed with 2 on the right. 
-        area is formed with the min value
-        '''
-        
-        
-        i = 0 
+        ret = -math.inf
+
+        i = 0
         j = len(height) - 1
-        ret = 0
+
         while i < j:
-            area = min(height[i],height[j]) * (j - i)
-            ret = max(ret,area)
+            area = min(height[i], height[j]) * (j-i)
+            ret = max(ret, area)
+
             if height[i] == height[j]:
                 i += 1
                 j -= 1
@@ -29,9 +16,4 @@ class Solution:
                 j -= 1
             else:
                 i += 1
-        
         return ret
-            
-        
-        
-        
