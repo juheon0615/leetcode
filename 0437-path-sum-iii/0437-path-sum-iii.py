ceb4e2a95f8d696a8b978b4ff4821668d70436f0
juheon0615 <juheon0615@gmail.com>
2024-10-21 13:30:52 +0900

    Time: 5 ms (99.06%), Space: 17.2 MB (26.42%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,39 +6,70 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        
-        '''
-        dfs for all nodes
-        this way, the path will aways be unique because all dfs start path will have different start node
-        '''
-        
-        ret = []
-        def dfs(node, cur):
-            # print(node.val, " : ", cur)
-            cur += node.val
+        def dfs(node, current_sum):
+            if not node:
+                return 0
             
-            if cur == targetSum:
-                ret.append(True)
+            # Update the current path sum by adding the node's value
+            current_sum += node.val
             
-            if node.left:
-                dfs(node.left, cur)
+            # Check if there is a prefix sum that we can subtract to get the targetSum
+            path_count = prefix_sums[current_sum - targetSum]
             
-            if node.right:
-                dfs(node.right, cur)
-        
-        
-        def runDfs(node):
-            dfs(node, 0)
+            # Update the prefix_sums map with the current sum
+            prefix_sums[current_sum] += 1
             
-            if node.left:
-                runDfs(node.left)
-            if node.right:
-                runDfs(node.right)
+            # Continue the DFS on left and right subtrees
+            path_count += dfs(node.left, current_sum)
+            path_count += dfs(node.right, current_sum)
+            
+            # After we're done with the node, backtrack: remove the current sum from the prefix_sums
+            prefix_sums[current_sum] -= 1
+            
+            return path_count
         
-        if root:
-            runDfs(root)
+        # Dictionary to store the frequency of prefix sums
+        prefix_sums = defaultdict(int)
+        # Initialize prefix sum 0 with count 1 to handle the case where path equals targetSum
+        prefix_sums[0] = 1
         
-        return len(ret)
+        # Start DFS traversal from the root with an initial cumulative sum of 0
+        return dfs(root, 0)
+
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        
+#         '''
+#         dfs for all nodes
+#         this way, the path will aways be unique because all dfs start path will have different start node
+#         '''
+        
+#         ret = []
+#         def dfs(node, cur):
+#             # print(node.val, " : ", cur)
+#             cur += node.val
+            
+#             if cur == targetSum:
+#                 ret.append(True)
+            
+#             if node.left:
+#                 dfs(node.left, cur)
+            
+#             if node.right:
+#                 dfs(node.right, cur)
+        
+        
+#         def runDfs(node):
+#             dfs(node, 0)
+            
+#             if node.left:
+#                 runDfs(node.left)
+#             if node.right:
+#                 runDfs(node.right)
+        
+#         if root:
+#             runDfs(root)
+        
+#         return len(ret)
         
         
                 
