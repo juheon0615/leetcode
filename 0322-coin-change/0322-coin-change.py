8ce89c8388feb1d5cf107b36cc2876294c83448f
juheon0615 <juheon0615@gmail.com>
2025-03-06 20:35:01 +0900

    Time: 479 ms (96.07%), Space: 18 MB (87.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,9 +3,9 @@ class Solution:
         dp = [None for _ in range(amount+1)]
         dp[0] = 0
         
-        for i in range(amount+1):
-            for coin in coins:
-                if coin <= i and dp[i - coin] is not None:
+        for coin in coins:
+            for i in range(coin, amount+1):
+                if dp[i - coin] is not None:
                     dp[i] = dp[i-coin] + 1 if dp[i] is None else min(dp[i], dp[i-coin] + 1)
         
         return dp[amount] if dp[amount] is not None else -1
