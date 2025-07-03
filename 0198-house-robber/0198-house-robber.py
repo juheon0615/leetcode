a4daebc6bfab83d7453e63d081e9102c55b7cbd5
juheon0615 <juheon0615@gmail.com>
2025-07-03 13:48:44 +0900

    Time: 0 ms (100%), Space: 17.9 MB (13.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(2, len(nums)):
             nums[i] = max(nums[i-1], nums[i-2] + nums[i])
         
-        print(nums)
         return max(nums[-1], nums[-2])
 
 
