d17bed1e8b3f42bf50de765d0a21f9fd5be526a2
juheon0615 <juheon0615@gmail.com>
2025-09-18 12:49:31 +0900

    Time: 13 ms (82.26%), Space: 18.8 MB (11.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,27 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        q = []
-        visited = set()
-        ret = False
-        
-        q.append(0)
-        visited.add(0)
-        while q:
-            i = q.pop(0)
-            j = nums[i]
-            
-            if i + j >= len(nums) - 1:
-                ret = True
-                break
-            else:
-                for jj in range(j):
-                    ii = i + jj + 1
-                    if ii not in visited:
-                        q.append(ii)
-                        visited.add(ii)
-            
-            
-         
-        return ret
-        
+        ret = len(nums) - 1
+
+        for i in range(len(nums)-1, -1, -1):
+            if i + nums[i] >= ret:
+                ret = i
+
+        return ret == 0
+
         
