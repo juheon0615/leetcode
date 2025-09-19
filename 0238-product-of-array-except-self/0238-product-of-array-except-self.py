2b359c07e583e1729d6ba578b4b1f8ab17a0f39c
juheon0615 <juheon0615@gmail.com>
2025-09-19 15:41:24 +0900

    Time: 46 ms (9.32%), Space: 27.1 MB (11.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,26 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ltr = [0 for _ in range(n)]
+        rtl = [0 for _ in range(n)]
+        ret = [0 for _ in range(n)]
+
+        ltr[0] = nums[0]
+        for i in range(1, n):
+            ltr[i] = ltr[i-1] * nums[i]
         
-        N = len(nums)
-        lr = [num for num in nums]
-        rl = [num for num in nums]
+        rtl[n-1] = nums[n-1]
+        for i in range(n - 2, -1, -1):
+            rtl[i] = rtl[i+1] * nums[i]
         
-        
-        for i in range(1, N):
-            lr[i] = lr[i-1] * lr[i]
-        
-        for i in range(N - 2, -1, -1):
-            rl[i] = rl[i+1] * rl[i]
-        
-        
-        for i in range(0, N):
-            left = 1 if i == 0 else lr[i-1]
-            right = 1 if i == N - 1 else rl[i+1]
-            
-            nums[i] = left * right
-            
-            
-        return nums
-            
-        
-        
+        ret[0] = rtl[1]
+        ret[n-1] = ltr[n-2]
+
+        for i in range(1, n-1):
+            ret[i] = ltr[i-1] * rtl[i+1]
+
+        return ret
