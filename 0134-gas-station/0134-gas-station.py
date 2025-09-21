c6122bdff904289f624ea1ad1bc7587f9595c168
juheon0615 <juheon0615@gmail.com>
2025-09-21 18:48:01 +0900

    Time: 43 ms (13.76%), Space: 24.4 MB (5.28%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,19 +1,23 @@
 class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int: # 17
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        nets = [gas[i] - cost[i] for i in range(n)]
+
+        if sum(nets) < 0:
+            return -1
         
-        total_sum = 0
-        cur_sum = 0
-        ans = 0
-        for i in range(len(gas)):
-            total_sum += gas[i] - cost[i]
-            cur_sum += gas[i] - cost[i]
-            
-            if cur_sum < 0:
-                cur_sum = 0 
-                ans = i + 1
-        
-        # print(total_sum)
-        return ans if total_sum >= 0 else -1
-        
-            
+        acc = 0
+        ret = None
+        for i in range(n):
+            if acc + nets[i] >= 0:
+                # dont update
+                ret = i if ret is None else ret
+                acc += nets[i]
+            else:
+                ret = None
+                acc = 0
+
+
+        print(nets)
+        return ret
         
