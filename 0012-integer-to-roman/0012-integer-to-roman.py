8497b93bc5164d501601eaadffb0cac3fdf4e629
juheon0615 <juheon0615@gmail.com>
2025-09-24 19:50:57 +0900

    Time: 4 ms (64.04%), Space: 17.8 MB (44.69%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,17 +1,22 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        vals = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        syms = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        
+        integers = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        romans = ["M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
         ret = ""
-        while num > 0:
-            for i, val in enumerate(vals):
-                if num >= val:
-                    mult = num // val
-                    ret += (syms[i] * mult)
-                    num -= (val * mult)
-                    
-                    
-        
+        for i in range(len(integers)):
+            if num == 0:
+                break
+            
+            integer = integers[i]
+            roman = romans[i]
+
+            d = num // integer
+            num = num % integer
+
+            ret += roman * d
+
         return ret
+
+
         
