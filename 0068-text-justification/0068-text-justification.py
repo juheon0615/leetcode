0bbd822867e8694e992ae13a7716136db3cdaba9
juheon0615 <juheon0615@gmail.com>
2025-09-27 17:42:42 +0900

    Time: 0 ms (100%), Space: 18 MB (19.37%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,60 +1,60 @@
-class Solution:#57
+class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        '''
-        specification to clairfy
-        1. about the inputs. i.e if word is always shorter than maxWidth
-        2. what if only one word can fit in the text? how do you space it?
-        '''
-        
-        texts = []
-        cur = ""
+
+        curWidth = 0
+        curLine = []
+        lines = []
         for word in words:
-            if len(cur) == 0:
-                cur = word
+            if len(word) + curWidth + len(curLine) - 1 < maxWidth:
+                curLine.append(word)
+                curWidth += len(word) 
             else:
-                if len(cur) + len(word) + 1 <= maxWidth:
-                    cur += " " + word
-                else:
-                    texts.append(cur)
-                    cur = word
-        texts.append(cur)
-        # print(texts)
-        for i, text in enumerate(texts):
-            if i == len(texts) - 1:
-                texts[i] = text + ((maxWidth - len(text)) * " ")
-            else:
-                if len(text) < maxWidth:                
-                    words = text.split(" ")
+                lines.append([curLine, curWidth])
+                curLine = []
+                curWidth = len(word)
+                curLine.append(word)
+        
+        if len(curLine) > 0:
+            lines.append([curLine, curWidth])
 
-                    if len(words) == 1:
-                        texts[i] = words[0] + (" " * (maxWidth - len(text)))
+        returnArray = []
+        for i in range(len(lines)):
+            line, width = lines[i]
+            numWords = len(line)
+
+            if i == len(lines) - 1:
+                text = ""
+                for j in range(len(line)):
+                    if j == len(line) - 1:
+                        text += line[j]
+                        spacer = " " * (maxWidth- len(text))
+                        text += spacer
                     else:
-                        spaces = maxWidth - len(text) + len(words) - 1
+                        text += line[j] + " "
+                returnArray.append(text)
+            elif numWords == 1:
+                spacer = " "  * (maxWidth - width)
+                text = line[0] + spacer
+                returnArray.append(text)
+            else:
+                spacerWidth = (maxWidth - width) // (numWords - 1)
+                extraSpacerIndex = (maxWidth - width) % (numWords - 1)
+                text = ""
+                for j in range(len(line)):
+                    if j == len(line) - 1:
+                        text += line[j]
+                    else:
+                        spacer = " " * spacerWidth
+                        text += line[j] + spacer
+                    
+                    if j < extraSpacerIndex:
+                        text += " "
+                returnArray.append(text)
 
-                        distribute = spaces // (len(words) - 1)
-                        remainder = spaces % (len(words) - 1)
 
-                        adjusted = ""
-                        for j in range(len(words)):
-                            if j == len(words) - 1:
-                                adjusted += words[j]
-                            else:
-                                adjusted += (words[j] + (" " * distribute))
-                                if j < remainder:
-                                    adjusted += " "
-                        texts[i] = adjusted
+
+        return returnArray
+
             
-        # print(texts)
-        return texts
-                        
-                        
-                    
-                    
-                
-                
-                
-                
-                    
-                
-                
-            
+        
+        
