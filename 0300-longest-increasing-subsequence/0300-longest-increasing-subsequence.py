5bbac2f52cf1b9905b6b133d8abe7960ca62a11f
juheon0615 <juheon0615@gmail.com>
2025-10-02 13:11:45 +0900

    Time: 2454 ms (12.01%), Space: 18.3 MB (18.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,46 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        lis = [1 for _ in range(len(nums))]
-        
-        
-        for i in range(1, len(nums)):
-            for j in range(0, i):
-                # print("i : ", i , " j: ", j, "  ", lis)
-                if nums[i] > nums[j] and lis[i] < lis[j] + 1:
-                    lis[i] = lis[j] + 1
-        
-        
-        ret = lis[0]
-        
-        for n in lis:
-            if n > ret:
-                ret = n
-        
-        return ret
-                
-        
-        
-#         answer = 0
-#         mem = [[-1 for prev in range(len(nums) + 1)] for i in range(len(nums))]
-#         def dp(i, prev):
-#             if i == len(nums):
-#                 return 0
-            
-#             if mem[i][prev+1] != -1:
-#                 return mem[i][prev+1]
-            
-#             ret = dp(i + 1, prev)
-#             if prev == -1 or nums[i] > nums[prev]:
-#                 ret = max(ret, dp(i + 1, i) + 1)
-            
-#             mem[i][prev+1] = ret
-            
-#             return ret 
-        
-#         answer = dp(0,-1)
-        
-#         return answer
-        
-        
-            
-        
+        n = len(nums)
+        dp = [1 for _ in range(n)]
+
+        for i in range(n):
+            j = 0
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
