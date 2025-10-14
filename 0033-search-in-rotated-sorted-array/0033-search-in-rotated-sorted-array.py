fee154e8887471c1531b14856e5c69d65da3311d
juheon0615 <juheon0615@gmail.com>
2025-10-14 13:55:23 +0900

    Time: 0 ms (100%), Space: 18.2 MB (41.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,35 +1,26 @@
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        '''
-        case left < right no pivot between left and right
-        - if mid == target return mid
-        - if mid > target search(left, mid - 1) else serach(mid + 1, right)
+        lo, hi = 0, len(nums) - 1
         
-        case left > right pivot between left and right 5, 6, 1, 2, 3, 4
-        - if mid == target return
-        - if mid < target and less than right serach(mid + 1, right)
-        - if mid < target and greater than right serach(left, mid - 1)
-        - if mid > target and mid - 1 and left
-        '''
-        
-        
-        def bsearch(left, right, nums, target):
-            if left > right:
-                return -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
             
-            mid = left + (right - left) // 2
-            
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                if nums[mid] < nums[left] and target > nums[right]:
-                    return bsearch(left, mid-1, nums, target)
+            
+            # Left side is sorted
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
                 else:
-                    return bsearch(mid+1, right, nums, target)
+                    lo = mid + 1
+            
+            # Right side is sorted
             else:
-                if nums[mid] > nums[right] and target < nums[left]:
-                    return bsearch(mid+1, right, nums, target)
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
                 else:
-                    return bsearch(left, mid-1, nums, target)
-        
-        return bsearch(0, len(nums)-1, nums, target)        
+                    hi = mid - 1
+                    
+        return -1
